63b52fc8a30dba9d43e75013c8e38761efce107c
Jayden Ma <jayden87@gmail.com>
2013-11-17 12:45:47 +0800

    Created pages controller and sample home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SlicingExercise::Application.routes.draw do
+  root :to => 'pages#home'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
