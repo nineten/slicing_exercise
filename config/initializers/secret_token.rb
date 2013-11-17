4e04ac3280a02518fdfbd737210c16a257b178b0
Jayden <jayden@InfinityDrive.(none)>
2013-11-17 12:38:15 +0800

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SlicingExercise::Application.config.secret_key_base = '1f2c9f61c0465861cf57be3d0eac4fd386ceb80047c79c577dea6c47c92daf1ae323e5f05f9071f2f8614bc70daa3e3e3d8e231d728c668bbd6621a8b9aebb05'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SlicingExercise::Application.config.session_store :cookie_store, key: '_slicing_exercise_session'
